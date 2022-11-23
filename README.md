d047f0b38082352867480592e2632ff71748559d
mebaysan <menesbaysan@gmail.com>
2022-11-23 10:34:20 +0300

    main concept is defined

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 I've created this repo to present a minimal crisis map for earhquakes in Turkey.
 
-# Using
+You can easily run the app by using [Docker](https://docker.com).
+
+
+Thanks for [BOGAZICI UNIVERSITY KANDILLI OBSERVATORY AND EARTHQUAKE RESEARCH INSTITUTE (KOERI)](http://www.koeri.boun.edu.tr/scripts/lasteq.asp) sharing the data.
+
+**Upon use of our data, proper attribution should be given to KOERI-RETMC in scientific articles and general purpose reports by referencing the network citation.**
+
+# Docker Image on the Hub
+Explore Image on Docker Hub: [mebaysan/turkey-earthquake-crisis-app](https://hub.docker.com/repository/docker/mebaysan/turkey-earthquake-crisis-app)
+
+# Manual
 
 ```bash
-
--p 8501:8501
-
+docker run -p 8501:8501 mebaysan/turkey-earthquake-crisis-app
 ```
--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
-"""
-# Introduction
-
-I've created this repo to present a minimal crisis map for earhquakes in Turkey.
-
-"""
-
 import streamlit as st
 import pandas as pd
 from scrapers.rasathane import get_json_data
 
 eq_data = pd.DataFrame(get_json_data())
+
+st.write(
+    """
+    # Introduction
+
+    I've created this repo to present a minimal crisis map for earhquakes in Turkey.
+    Thanks for [BOGAZICI UNIVERSITY KANDILLI OBSERVATORY AND EARTHQUAKE RESEARCH INSTITUTE (KOERI)](http://www.koeri.boun.edu.tr/scripts/lasteq.asp) sharing the data.
+
+    **Upon use of our data, proper attribution should be given to KOERI-RETMC in scientific articles and general purpose reports by referencing the network citation.**
+
+    # Docker Image on the Hub
+    
+    Explore Image on Docker Hub: [mebaysan/turkey-earthquake-crisis-app](https://hub.docker.com/repository/docker/mebaysan/turkey-earthquake-crisis-app)
+
+"""
+)
 
 st.write(eq_data)
